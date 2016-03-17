1cd846c9c12c50abd4660aa1e2d4774739923d50
Ari Scott <ariscott12@gmail.com>
2016-03-17 10:38:24 -0600

    created <Zone /> component for rendering UI for each zone, uses react widgets to create number picker for setting timer, implements zone-start-store for API calls

--- js/components/zones.js
+++ js/components/zones.js
@@ -1,34 +1,67 @@
 const React = require('react');
 const Reflux = require('reflux');
+const Api = require('../utils/api');
+const Timer = require('./timer');
+const NumberPicker = require('react-widgets/lib/NumberPicker');
+const numberLocalizer = require('react-widgets/lib/localizers/simple-number');
+const ZoneStartStore = require('../stores/zone-start-store');
+numberLocalizer();
+
 
 module.exports = React.createClass({
+	mixins: [
+	    // listens for any events that are coming from ZoneStore, 
+	    // call onChange function when event triggered by store
+	    Reflux.listenTo(ZoneStartStore, 'onStartSingle')
+  	],
 	getInitialState() {
 		return {
-			time: null
+			duration:1,
+			timer: false,
+			timerDuration: 0
 		}
+	},
+	startZone() {
+		// duration must be an integer
+		let duration = parseInt(this.state.duration);
+		let id = this.props.id;
+
+		// Set timer back to false in case it's already running, set number picker back to 1
+		this.setState({
+			duration: 1,
+			timer: false
+		})
+
+		// Make API put call, takes id and duration
+		ZoneStartStore.startZone(id, duration);
+	},
+	onStartSingle(event,id,duration) {
+		if(id === this.props.id) {	
+			this.setState({
+            	timer: true,
+            	timerDuration: duration
+        	});
+		}
+	},
+	showTimer() {
+		return <Timer secondsRemaining={this.state.timerDuration} />
+	},
+	handleChange(value) {
+		this.setState({
+        	duration: value
+      	});
+	},
+	renderForm() {
+   		return <div>
+	   		 <NumberPicker  max={180} min={1} value={this.state.duration} onChange={this.handleChange}/>
+	   		 <button onClick={this.startZone}>Start Zone</button>
+	   		 {this.state.timer ? this.showTimer() : null}
+   		</div>
 	},
 	render() {
 		return <div>
 			<p>Zone Number { this.props.zoneNumber }</p>
 			{this.renderForm()}
 		</div>
-	},
-	renderForm() {
-		 let time = this.state.time;
-   		 return <div>
-   		 <input type="text" value={time} onChange={this.handleChange} />
-   		 <button onClick={this.startZone}>Start Zone</button>
-   		 </div>
-	},
-	startZone(event) {
-		console.log(this.state.time);
-		this.setState({
-			time:null
-		})
-	},
-	handleChange(event) {
-		this.setState({
-			time: event.target.value
-		})
 	}
 })

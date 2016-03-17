b8786d1bdb47a070b7378cb5c6b124c77fb94ccf
Ari Scott <ariscott12@gmail.com>
2016-03-17 10:35:13 -0600

    added put function to api module for starting single zone, takes id, and optional duration and url

--- js/utils/api.js
+++ js/utils/api.js
@@ -1,16 +1,28 @@
 const Fetch = require('whatwg-fetch');
-const RootUrl =  'https://api.rach.io/1/public/';
+const RootUrl = 'https://api.rach.io/1/public/';
 const ApiKey = 'Bearer c3667b81-92a6-4913-b83c-64cc713cbc1e';
-
 module.exports = {
-	get(url) {
-		return fetch(RootUrl + url, {
-			headers: {
-				'Authorization' : ApiKey
-			}
-		})
-		.then(function(response) {
-			return response.json();
-		})
-	}
+    get(url) {
+        return fetch(RootUrl + url, {
+            headers: {
+                'Authorization': ApiKey
+            }
+        }).then(function(response) {
+            return response.json();
+        })
+    },
+    put(id, duration = 60, url = 'zone/start') {
+        return fetch(RootUrl + url, {
+            method: 'PUT',
+            headers: {
+                'Authorization': ApiKey
+            },
+            body: JSON.stringify({
+                id: id,
+                duration: duration,
+            })
+        }).then(function(response) {
+        	return response
+        })
+    }
 }

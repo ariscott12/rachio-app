bdfa596180b5b77b6bafe69db5cc2e11955f1c46
Ari Scott <ariscott12@gmail.com>
2016-03-18 14:28:03 -0600

    updated startZone function, takes body argument, passes zone/start_multiple to API

--- js/stores/zone-start-store.js
+++ js/stores/zone-start-store.js
@@ -3,21 +3,17 @@ const Api = require('../utils/api');
 const Reflux = require('reflux');
 
 module.exports = Reflux.createStore({
-  startSingleZone(id,duration) {
-      this.id = id;
-      this.duration = duration;
-
-      // Takes optional 3rd argument to set url
-      Api.put(id,duration)
+  startZones(body) {   
+      Api.put('zone/start_multiple', body)
         .then(function(response) {
           // If request was valid
           if(response.ok === true) {
-            this.triggerStartSingle()
+            this.triggerChange();
           }
         }.bind(this));
   },
-  triggerStartSingle() {
-    this.trigger('startSingle', this.id, this.duration);
+  triggerChange() {
+    this.trigger('change');
   }
 })
  

f65f2b0f5532fc1bb7902a2bc3d075014a3842ea
Ari Scott <ariscott12@gmail.com>
2016-03-18 09:22:22 -0600

    commented out API call since it will be made in parent component, removed start button since there will be 1 master start button, timer also no longer needed in <Zone / > component

--- js/components/zones.js
+++ js/components/zones.js
@@ -17,51 +17,69 @@ module.exports = React.createClass({
 	getInitialState() {
 		return {
 			duration:1,
-			timer: false,
-			timerDuration: 0
+			//timer: false,
+			timerDuration: 0,
+			active: false
 		}
 	},
-	startZone() {
-		// duration must be an integer
-		let duration = parseInt(this.state.duration);
-		let id = this.props.id;
+	// startZone() {
+	// 	// duration must be an integer
+	// 	let duration = parseInt(this.state.duration);
+	// 	let id = this.props.id;
 
-		// Set timer back to false in case it's already running, set number picker back to 1
-		this.setState({
-			duration: 1,
-			timer: false
-		})
+	// 	// Set timer back to false in case it's already running, set number picker back to 1
+	// 	this.setState({
+	// 		duration: 1,
+	// 		timer: false
+	// 	})
 
-		// Make API put call, takes id and duration
-		ZoneStartStore.startZone(id, duration);
-	},
-	onStartSingle(event,id,duration) {
-		if(id === this.props.id) {	
-			this.setState({
-            	timer: true,
-            	timerDuration: duration
-        	});
-		}
-	},
-	showTimer() {
-		return <Timer secondsRemaining={this.state.timerDuration} />
-	},
+	// 	// Make API put call, takes id and duration
+	// 	ZoneStartStore.startSingleZone(id, duration);
+	// },
+	// onStartSingle(event,id,duration) {
+	// 	if(id === this.props.id) {	
+	// 		this.setState({
+ //            	timer: true,
+ //            	timerDuration: duration
+ //        	});
+	// 	}
+	// },
+	// showTimer() {
+	// 	return <Timer secondsRemaining={this.state.timerDuration} />
+	// },
 	handleChange(value) {
 		this.setState({
         	duration: value
       	});
+      	if(this.state.active) {
+      		this.props.onUpdate(this.props.id, value);
+      	}
+	},
+	toggleActive() {
+		if(this.state.active) {
+			this.setState({
+				active: false
+			});
+		} else {
+			this.setState({
+				active: true
+			});
+		}
+		this.props.onActivate(this.props.id, this.state.duration);
+		
+		
 	},
 	renderForm() {
    		return <div>
-	   		 <NumberPicker  max={180} min={1} value={this.state.duration} onChange={this.handleChange}/>
-	   		 <button onClick={this.startZone}>Start Zone</button>
-	   		 {this.state.timer ? this.showTimer() : null}
+	   		<NumberPicker  max={180} min={1} value={this.state.duration} onChange={this.handleChange}/>
+	   		{this.state.timer ? this.showTimer() : null}
+	   		<p>Set Time (mins)</p>
    		</div>
 	},
 	render() {
 		return <div>
-			<p>Zone Number { this.props.zoneNumber }</p>
-			{this.renderForm()}
+			<div onClick={this.toggleActive}>Zone Number { this.props.zoneNumber }</div>
+			{ this.state.active ? this.renderForm() : null }
 		</div>
 	}
 })

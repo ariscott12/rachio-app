470575226e0c0c0481f370adce6720597c2313e6
Ari Scott <ariscott12@gmail.com>
2016-03-18 14:31:54 -0600

    cleaned up zone module, listens to zone-start-store onChange event, sets active state to false when API put call complete

--- js/components/zones.js
+++ js/components/zones.js
@@ -1,78 +1,45 @@
 const React = require('react');
-const Reflux = require('reflux');
-const Api = require('../utils/api');
-const Timer = require('./timer');
 const NumberPicker = require('react-widgets/lib/NumberPicker');
 const numberLocalizer = require('react-widgets/lib/localizers/simple-number');
+const Reflux = require('reflux');
 const ZoneStartStore = require('../stores/zone-start-store');
 numberLocalizer();
 
 
 module.exports = React.createClass({
 	mixins: [
-	    // listens for any events that are coming from ZoneStore, 
-	    // call onChange function when event triggered by store
-	    Reflux.listenTo(ZoneStartStore, 'onStartSingle')
+	    Reflux.listenTo(ZoneStartStore, 'onChange')
   	],
+  	onChange(event) {
+		this.setState({
+			active:false
+		})
+	},
 	getInitialState() {
 		return {
 			duration:1,
-			//timer: false,
-			timerDuration: 0,
 			active: false
 		}
 	},
-	// startZone() {
-	// 	// duration must be an integer
-	// 	let duration = parseInt(this.state.duration);
-	// 	let id = this.props.id;
-
-	// 	// Set timer back to false in case it's already running, set number picker back to 1
-	// 	this.setState({
-	// 		duration: 1,
-	// 		timer: false
-	// 	})
-
-	// 	// Make API put call, takes id and duration
-	// 	ZoneStartStore.startSingleZone(id, duration);
-	// },
-	// onStartSingle(event,id,duration) {
-	// 	if(id === this.props.id) {	
-	// 		this.setState({
- //            	timer: true,
- //            	timerDuration: duration
- //        	});
-	// 	}
-	// },
-	// showTimer() {
-	// 	return <Timer secondsRemaining={this.state.timerDuration} />
-	// },
 	handleChange(value) {
 		this.setState({
         	duration: value
       	});
+
       	if(this.state.active) {
       		this.props.onUpdate(this.props.id, value);
       	}
 	},
 	toggleActive() {
-		if(this.state.active) {
 			this.setState({
-				active: false
+				active: !this.state.active
 			});
-		} else {
-			this.setState({
-				active: true
-			});
-		}
+		
 		this.props.onActivate(this.props.id, this.state.duration);
-		
-		
 	},
 	renderForm() {
    		return <div>
 	   		<NumberPicker  max={180} min={1} value={this.state.duration} onChange={this.handleChange}/>
-	   		{this.state.timer ? this.showTimer() : null}
 	   		<p>Set Time (mins)</p>
    		</div>
 	},

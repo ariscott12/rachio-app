d84b584e3960aece2dc49989eebb90332074c34a
Ari Scott <ariscott12@gmail.com>
2016-03-21 10:36:34 -0600

    removed file from git since it had api key hard coded, moved api key into config file so it won't be checked into repository

---
+++ js/utils/api.js
@@ -0,0 +1,27 @@
+const Fetch = require('whatwg-fetch');
+const RootUrl = 'https://api.rach.io/1/public/';
+const config = require('../config.js');
+
+module.exports = {
+    get(url) {
+        return fetch(RootUrl + url, {
+            headers: {
+                'Authorization': config.apiKey
+            }
+        }).then(function(response) {
+            return response.json();
+        });
+    },
+    put(url, body) {
+    	body = JSON.stringify(body);
+        return fetch(RootUrl + url, {
+            method: 'PUT',
+            headers: {
+                'Authorization': ApiKey
+            },
+            body: body
+        }).then(function(response) {
+        	return response;
+        });
+    }
+}
